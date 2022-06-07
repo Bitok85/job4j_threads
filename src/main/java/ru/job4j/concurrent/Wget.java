70e6d382aa87fc1e775da9deac9069cbea1e1c40
Bitok85 <kan041185@gmail.com>
2022-06-07 13:33:54 +0300

    4. Скачивание файла с ограничением. [#144271]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -1,19 +1,43 @@
 package ru.job4j.concurrent;
 
-public class Wget {
+import java.io.FileOutputStream;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+
+public class Wget implements Runnable {
+
+    private final URL url;
+    private final int speed;
+
+    public Wget(URL url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        try (ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+             FileOutputStream fos = new FileOutputStream("pom_tmp.xml")) {
+                 fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+                 Thread.sleep(speed);
+             } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        Thread thread = new Thread(
-                () -> {
-                    try {
-                        for (int i = 1; i <= 100; i++) {
-                            System.out.println("\rLoading : " + i + "%");
-                            Thread.sleep(1000);
-                        }
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-        );
-        thread.start();
+        try {
+            Thread wget = new Thread(new Wget(
+                    new URL("https://raw.githubusercontent.com/peterarsentev/course_test/master/pom.xml"),
+                    1000
+            ));
+            wget.start();
+            wget.join();
+            System.out.println(wget.getState());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
