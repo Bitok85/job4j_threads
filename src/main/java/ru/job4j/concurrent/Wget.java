6491da2ed00fa00a082c8d2288542b40a569c66f
Bitok85 <kan041185@gmail.com>
2022-06-08 14:07:51 +0300

    4. Скачивание файла с ограничением. [#144271 #310001]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -1,43 +1,66 @@
 package ru.job4j.concurrent;
 
+import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
 import java.net.URL;
-import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
 
 public class Wget implements Runnable {
 
-    private final URL url;
-    private final int speed;
+    private static final int MILLS_PAUSE = 1000;
 
-    public Wget(URL url, int speed) {
+    private final String url;
+    private final int speed;
+    private final String dest;
+
+    public Wget(String url, int speed, String dest) {
         this.url = url;
         this.speed = speed;
+        this.dest = dest;
     }
 
     @Override
     public void run() {
-        try (ReadableByteChannel rbc = Channels.newChannel(url.openStream());
-             FileOutputStream fos = new FileOutputStream("pom_tmp.xml")) {
-                 fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-                 Thread.sleep(speed);
-             } catch (Exception e) {
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+            FileOutputStream out = new FileOutputStream(dest)) {
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead;
+            long bytesWrited = 0;
+            long startTime = System.currentTimeMillis();
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                out.write(dataBuffer, 0, bytesRead);
+                bytesWrited += bytesRead;
+                if (bytesWrited >= speed) {
+                    long endTime = System.currentTimeMillis();
+                    if (endTime - startTime < MILLS_PAUSE) {
+                        Thread.sleep(MILLS_PAUSE - (endTime - startTime));
+                    }
+                    bytesWrited = 0;
+                    startTime = System.currentTimeMillis();
+                }
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    private static void validateArgs(String[] arr) {
+        if (arr.length != 3) {
+            throw new IllegalArgumentException("You have to enter 3 params: \n"
+                    + "1. file url \n"
+                    + "2. speed of downloading per second \n"
+                    + "3. writing path \n");
+        }
+    }
+
     public static void main(String[] args) {
+        validateArgs(args);
         try {
-            Thread wget = new Thread(new Wget(
-                    new URL("https://raw.githubusercontent.com/peterarsentev/course_test/master/pom.xml"),
-                    1000
-            ));
+            Thread wget = new Thread(new Wget(args[0], Integer.parseInt(args[1]), args[2]));
             wget.start();
             wget.join();
             System.out.println(wget.getState());
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 }
