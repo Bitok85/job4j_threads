2cc65d6a47ed4b06c6ceafcf7b1d2e2cba2c9b1a
Bitok85 <kan041185@gmail.com>
2022-06-10 09:23:40 +0300

    1. Visibility. Общий ресурс вне критической секции [#1102]

---
+++ src/main/java/ru/job4j/concurrent/ParseFile.java
@@ -0,0 +1,40 @@
+package ru.job4j.concurrent;
+
+import java.io.*;
+import java.util.function.Predicate;
+
+public class ParseFile {
+
+    private final FileModel file;
+    private final File destFile;
+
+    public ParseFile(FileModel file, File destFile) {
+        this.file = file;
+        this.destFile = destFile;
+    }
+
+    public String getContent(Predicate<Character> filter) throws IOException {
+        String result;
+        try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(file.getFile()))) {
+            StringBuilder output = new StringBuilder();
+            int data;
+            while ((data = in.read()) > 0) {
+                if (filter.test((char) data)) {
+                    output.append((char) data);
+                }
+            }
+            result = output.toString();
+        }
+        return result;
+    }
+
+    public void saveContent(String content) throws IOException {
+        try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(destFile))) {
+            for (int i = 0; i < content.length(); i++) {
+                out.write(content.charAt(i));
+            }
+        }
+    }
+
+
+}
