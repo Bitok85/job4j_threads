bf7f5031dde53050ab4a9d74e58282d9c586db2c
Bitok85 <kan041185@gmail.com>
2022-07-01 11:18:58 +0300

    2. ExecutorService рассылка почты. [#63097 #317203]

--- src/main/java/ru/job4j/concurrent/pool/EmailNotification.java
+++ src/main/java/ru/job4j/concurrent/pool/EmailNotification.java
@@ -6,23 +6,21 @@ import java.util.concurrent.Executors;
 
 public class EmailNotification {
 
-    private final List<User> users;
     private final String message;
 
     private ExecutorService pool = Executors.newFixedThreadPool(
             Runtime.getRuntime().availableProcessors()
     );
 
-    public EmailNotification(List<User> users, String message) {
-        this.users = users;
+    public EmailNotification(String message) {
         this.message = message;
     }
 
-    public void emailTo() {
-        users.forEach(user -> pool.submit(() -> {
+    public void emailTo(User user) {
+        pool.submit(() -> {
             String[] sample = new Sample(user).mailArr();
             send(sample[0], sample[1], message);
-        }));
+        });
         close();
     }
 
