f13de56dbd2967cac49f18956cf34ea4d1d290bc
Bitok85 <kan041185@gmail.com>
2022-07-06 13:00:52 +0300

    3. ForkJoinPool [#315067 #318036]

--- src/main/java/ru/job4j/concurrent/forkjoin/ParallelLinearSearch.java
+++ src/main/java/ru/job4j/concurrent/forkjoin/ParallelLinearSearch.java
@@ -1,11 +1,9 @@
 package ru.job4j.concurrent.forkjoin;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
-public class ParallelLinearSearch<T> extends RecursiveTask<List<Integer>> {
+public class ParallelLinearSearch<T> extends RecursiveTask<Integer> {
 
     private static final int MIN_ARR_LENGTH = 10;
 
@@ -22,24 +20,30 @@ public class ParallelLinearSearch<T> extends RecursiveTask<List<Integer>> {
     }
 
     @Override
-    protected List<Integer> compute() {
+    protected Integer compute() {
         if (to - from <= MIN_ARR_LENGTH) {
-            List<Integer> tmpList = new ArrayList<>();
             for (int i = from; i < to; i++) {
                 if (value.equals(array[i])) {
-                    tmpList.add(i);
+                    return i;
                 }
             }
-            return tmpList;
         }
-        int mid = array.length / 2;
+        int mid = (to - from) / 2;
         ParallelLinearSearch<T> leftSearch = new ParallelLinearSearch<>(array, value, from, mid);
         ParallelLinearSearch<T> rightSearch = new ParallelLinearSearch<>(array, value, mid + 1, to);
         leftSearch.fork();
         rightSearch.fork();
-        List<Integer> rsl = new ArrayList<>();
-        rsl.addAll(leftSearch.join());
-        rsl.addAll(rightSearch.join());
+        return resultChoice(leftSearch.join(), rightSearch.join());
+    }
+
+
+    private static int resultChoice(Integer left, Integer right) {
+        int rsl = -1;
+        if (left != null) {
+            rsl = left;
+        } else if (right != null) {
+            rsl = right;
+        }
         return rsl;
     }
 
